a4a6df4582aa5d3393835ad85d86f83af4c36b10
gstevenvalgreen <gstevenvalverde@gmail.com>
2023-07-12 14:17:32 -0400

    Se tiene que continuar editando beneficiario-registro

--- js/form-usuario.js
+++ js/form-usuario.js
@@ -1,27 +1,24 @@
 const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input');
 
+
 const expresiones = {
-	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
 	password: /^.{4,12}$/, // 4 a 12 digitos.
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+	ci: /^\d{7,14}$/, // 7 a 14 numeros.
 }
 
 const campos = {
-	usuario: false,
 	nombre: false,
 	password: false,
 	correo: false,
-	telefono: false
+	ci: false,
+	rol: false,
 }
 
 const validarFormulario = (e) => {
 	switch (e.target.name) {
-		case "usuario":
-			validarCampo(expresiones.usuario, e.target, 'usuario');
-		break;
 		case "nombre":
 			validarCampo(expresiones.nombre, e.target, 'nombre');
 		break;
@@ -35,13 +32,14 @@ const validarFormulario = (e) => {
 		case "correo":
 			validarCampo(expresiones.correo, e.target, 'correo');
 		break;
-		case "telefono":
-			validarCampo(expresiones.telefono, e.target, 'telefono');
+		case "ci":
+			validarCampo(expresiones.ci, e.target, 'ci');
 		break;
 	}
 }
 
 const validarCampo = (expresion, input, campo) => {
+	console.log("Soy el " + campo);
 	if(expresion.test(input.value)){
 		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
 		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
@@ -80,6 +78,19 @@ const validarPassword2 = () => {
 	}
 }
 
+const validarRol = () => {
+	console.log("Validando...");
+	var rol = document.getElementById("rol");
+	console.log(rol);
+	if(rol.value == 0 || rol.value == "") {
+		campos['rol'] = false;
+		
+	} else {
+		campos['rol'] = true;
+	}
+	console.log(campos.rol);
+}
+
 inputs.forEach((input) => {
 	input.addEventListener('keyup', validarFormulario);
 	input.addEventListener('blur', validarFormulario);
@@ -87,9 +98,9 @@ inputs.forEach((input) => {
 
 formulario.addEventListener('submit', (e) => {
 	e.preventDefault();
-
+	validarRol()
 	const terminos = document.getElementById('terminos');
-	if(campos.usuario && campos.nombre && campos.password && campos.correo && campos.telefono && terminos.checked ){
+	if(campos.nombre && campos.password && campos.correo && campos.ci && campos.rol){
 		formulario.reset();
 
 		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
--- js/script.js
+++ js/script.js
@@ -19,18 +19,21 @@ changeTheme = function(){
   //var imgLogo = document.getElementById("logo");
   //var imgLogoFull = document.getElementById("logo-full");
   var btnTheme = document.getElementById("btn-change-theme");
+  var menuMode = document.getElementById("menu-mode");
 
   fileName = css.getAttribute("href");
   if(fileName == "/mi-rancho-front/css/dark.css"){
       css.setAttribute("href", "/mi-rancho-front/css/light.css");
       //imgLogo.src = "images/logo-light.png";
       //imgLogoFull.src = "images/logo-full-light.png";
-      btnTheme.innerHTML = '<i class="fas fa-moon"></i>';
+      btnTheme.innerHTML = '<i class="fas fa-moon"></i><span class="link_name">Night</span>';
+      menuMode.innerHTML = '<li><a class="link_name" href="#">Night</a></li>';
   }else{
       css.setAttribute("href", "/mi-rancho-front/css/dark.css");
       //imgLogo.src = "images/logo.png";
       //imgLogoFull.src = "images/logo-full.png";
-      btnTheme.innerHTML = '<i class="fas fa-sun"></i>';
+      btnTheme.innerHTML = '<i class="fas fa-sun"></i><span class="link_name">Light</span>';
+      menuMode.innerHTML = '<li><a class="link_name" href="#">Light</a></li>';
   }
 }
 
